b7f72a8a78e743adbecd05d28ff96c09df6d34ac
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2020-01-22 12:41:26 -0500

    Initial commit

---
+++ Assets/bouncy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//when player enters this zone, player bounces uppp
+
+public class bouncy : MonoBehaviour
+{
+    public float bounce_level = 500f * Time.deltaTime;
+    
+    public GameObject the_other_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<Rigidbody>().AddForce(0, bounce_level, 0);
+        //normal for bounce
+        //add *Time.deltaTime for floating effect
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //other.GetComponent<AudioSource>().Play();
+        if (other.gameObject == the_other_object)
+        {
+            other.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/rotatingg.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatingg : MonoBehaviour
+{
+    public float rotate_speed = 1.0f;
+    //set variable to public --> variable will be displayed in Component view in Unity Editor
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //this is to create game loops
+    void FixedUpdate()
+        //FixedUpdate guarantees that the frame will run 60fps, no matter how the render turns out
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            this.transform.Rotate(0, rotate_speed, 0);
+            //this.transform.Rotate(0,1.0f*Time.deltaTime,0); --> error apparently???
+            //convention = when you only write the number, you don't need to define the type of unit. But in any other case, you need to specify (f for floating point, d for double)
+            //what is floating point? what is double?
+            //Time. deltaTime is essentially a fragment of a second or the time passed since the last frame. So if you move something with it in involved it will move them by time rather than by frame.
+        }
+    }
+}
